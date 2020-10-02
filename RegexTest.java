f9de7db0691d03663a3912ce5216edf137d18ea8
batista12 <50904472+batista12@users.noreply.github.com>
2020-10-02 11:15:10 -0700

    Manasi Singh changes done to the UC2

---
+++ RegexTest.java
@@ -0,0 +1,14 @@
+package com.capgemini.helloworld;
+import java.util.Scanner;
+public class UserReigstration{
+	public static void main(String[] args){
+		try{
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the ");
+			String lname = sc.next();
+			String pattern = "[A-Z]*[a-zA-Z]{3,}$";
+			if(lname.matches(pattern))
+				System.out.println(lname);
+			else return;
+		}catch(Exception e){return;}}
+}
