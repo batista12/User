281835e51cb6b72fa20421730d47ebf566f92063
batista12 <50904472+batista12@users.noreply.github.com>
2020-10-04 20:26:41 -0700

    Update RegexTest.java

--- RegexTest.java
+++ RegexTest.java
@@ -23,27 +23,3 @@ public class UserRegistraion
 		}
   }
   
-public class UserReigstration{
-	public static void main(String[] args){
-		try{
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter the ");
-			String lname = sc.next();
-			String pattern = "[A-Z]*[a-zA-Z]{3,}$";
-			if(lname.matches(pattern))
-				System.out.println(lname);
-
-
-import java.util.Scanner;
-public class Hello{
-	public static void main(String[] args){
-		try{
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter the first name");
-			String fname = sc.next();
-			String pattern = "[A-Z]*[a-zA-Z]{3,}$";
-			if(fname.matches(pattern))
-				System.out.println(fname);
-			else return;
-		}catch(Exception e){return;}}
-}
