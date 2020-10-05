02cb0ec1029d8114477dec22ebda8fcfaf78c2e4
batista12 <50904472+batista12@users.noreply.github.com>
2020-10-04 20:29:48 -0700

    Update RegexTest.java

--- RegexTest.java
+++ RegexTest.java
@@ -6,7 +6,7 @@ public class userRegistration{
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Enter the phone ");
 	String phone = sc.next();
-	String pattern = "^[0/91]{2}[' ']{1}[789]{1}[0-9]{9}$";
+	String pattern = "^[A-Za-z0-9]+[+-_]*/w*[@][A-Za-z0-9]*[.][A-Za-z]*[.][A-Za-z]*";
 	if(phone.matches(pattern))
 	System.out.println(phone);
 	else return;
